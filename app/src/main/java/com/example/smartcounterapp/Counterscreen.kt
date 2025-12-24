1da5ce437a2139fa0d964c39743ac164e857a925
Farwa Usman <farwa333.u@gmail.com>
2025-12-23 19:35:00 -0800

    Scaffold addition

--- app/src/main/java/com/example/smartcounterapp/Counterscreen.kt
+++ app/src/main/java/com/example/smartcounterapp/Counterscreen.kt
@@ -1,16 +1,21 @@
 package com.example.smartcounterapp
 
+import android.R.attr.content
+import android.R.attr.padding
 import android.inputmethodservice.Keyboard
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -23,7 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
+//import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,26 +37,30 @@ import kotlinx.coroutines.launch
 @Preview(showSystemUi = true)
 @Composable
 fun CounterScreen(){
+    val message=remember { SnackbarHostState()}
     var counter by remember { mutableStateOf(0) }
     var scope = rememberCoroutineScope()
-    val message=remember { SnackbarHostState()}
-    var snack= SnackbarHost(hostState = message)
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally)
-    { Text("$counter", fontSize = 20.sp)
-    Spacer(modifier = Modifier.height(16.dp))
-   Row{
-       // Increment Button
-       Button(onClick = {counter++
-           if(counter==20) (scope.launch { message.showSnackbar("Maximum Limit reached", actionLabel = "Retry") })}, enabled = counter<20,
-        colors = ButtonDefaults.
-        buttonColors(containerColor = Color.Green))
-    {Text("Increase", fontSize = 18.sp)}
-       Spacer(modifier = Modifier.width(16.dp))
-       // Decrement Button
-    Button(onClick = {if (counter>0) counter--},
-        colors = ButtonDefaults.
-        buttonColors(containerColor = Color.Red))
-    {Text("Decrease", fontSize = 18.sp)} }}}
+    Scaffold (
+        topBar = {},
+      snackbarHost={SnackbarHost(hostState = message)},
+       content =  {  PaddingValues->
+          Column(modifier = Modifier.padding(PaddingValues).fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally)
+        { Text("$counter", fontSize = 20.sp)
+            Spacer(modifier = Modifier.height(16.dp))
+            Row{
+                // Increment Button
+                Button(onClick = {counter++
+                    if(counter==20) (scope.launch { message.showSnackbar("Maximum Limit reached", actionLabel = "Retry") })}, enabled = counter<20,
+                    colors = ButtonDefaults.
+                    buttonColors(containerColor = Color.Green))
+                {Text("Increase", fontSize = 18.sp)}
+                Spacer(modifier = Modifier.width(16.dp))
+                // Decrement Button
+                Button(onClick = {if (counter>0) counter--},
+                    colors = ButtonDefaults.
+                    buttonColors(containerColor = Color.Red))
+                {Text("Decrease", fontSize = 18.sp)} }}})}
+
 
